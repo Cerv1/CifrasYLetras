0c1d3dff1bda3ea08e1a860e3d617c73c0254853
Cerv1 <seryi_111@hotmail.es>
2017-06-13 15:27:20 +0200

    Back to the basic

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 
   require "common_page.php";
   HTMLbegin();
+  HTMLheader();
 
   if(!isset($_GET["p"]))
     $_GET['p']=0;
   else if($_GET["p"]<0 || $_GET["p"]>4)
     $_GET['p']=0;
-  HTMLheader($_GET["p"]);
+  HTMLnav($_GET["p"]);
   HTMLcontent($_GET["p"]);
   // include "content_page.html";
   HTMLfooter();
