7224d76b3a0188ab4bc5581c7cdc9fd5e434713d
Cerv1 <seryi_111@hotmail.es>
2017-06-15 10:36:05 +0200

    added config-db.php

---
+++ config-db.php
@@ -0,0 +1,7 @@
+<?php
+  define('DB_SERVER', 'localhost:8001');
+  define('DB_USERNAME', 'root');
+  define('DB_PASSWORD', 'twpassword');
+  define('DB_DATABASE', 'CifrasYLetras-DB');
+  $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+ ?>
