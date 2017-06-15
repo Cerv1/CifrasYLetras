7d66feedd321269e2eb916f850deddd9a0d9248e
Cerv1 <seryi_111@hotmail.es>
2017-06-15 11:43:55 +0200

    adding db connection

--- common_page.php
+++ common_page.php
@@ -326,6 +326,22 @@
       </div>
     </div>
     ';
+    $myusername = mysqli_real_escape_string($db, $_POST['user']);
+    $mypassword = mysqli_real_escape_string($db, $_POST['pass']);
+    $sql = "SELECT name FROM users WHERE name= '$myusername' AND password='$mypassword'";
+    $result = mysqli_queri($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $active = $row['active'];
 
+    $count = mysqli_num_rows($result);
+
+    if($count == 1){
+      session_register("mysuername");
+      $_SESSION['login_user'] = $myusername;
+
+      header("location: index.php");
+    }else{
+      $error = "Your Login Name or Password is invalid";
+    }
   }
 ?>
