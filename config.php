e05a8c5a36b8a5ad391db1f17e1a7ea63acf35f7
Adrian Morente <adrian95morente@gmail.com>
2017-06-15 12:23:43 +0200

    connecting DB

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  define('DB_SERVER', 'localhost');
+  define('DB_USERNAME', 'root');
+  define('DB_PASSWORD', 'twpassword');
+  define('DB_DATABASE', 'CifrasYLetrasDB');
+  $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+ ?>
