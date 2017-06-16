3e56ac2b555780ca2b35ba1f74ccf4d35c0e26ec
Cerv1 <cervick13@gmail.com>
2017-06-16 12:46:37 +0200

    fixed style

--- php/users_common_page.php
+++ php/users_common_page.php
@@ -360,7 +360,7 @@
       <div class="facilities-title"><h1>Lista de asignaturas y material</h1></div>
       <div class="gallery-content">
         <div  id="subject-content" class="gallery-text">
-          <p class="parrafo"><br>Aqui van las asignaturas</p>
+          <p class="subject-text"><br>Aqui van las asignaturas</p>
         </div>
         <div  class="subject-buttons">
           <input type="button"  class="login login-submit" name="createUser" value="Añadir alumno" />
