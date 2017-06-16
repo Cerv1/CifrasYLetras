9e1d43f7b56a416299bcf45475b4e89006ff1bb7
Adrian Morente <adrian95morente@gmail.com>
2017-06-16 11:17:54 +0200

    fixed queries!

--- php/database-methods.php
+++ php/database-methods.php
@@ -29,9 +29,8 @@
 
   function createUser($name, $lastname, $emailTeacher, $birth){
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-      $sql = "INSERT INTO users(name, lastname, emailTeacher, birth)
-              VALUES($name, $lastname, $emailTeacher, $birth)";
-      $result = mysqli_query($db,$sql);
+      $sql = "INSERT INTO users(name, lastname, emailTeacher, birth) VALUES($name, $lastname, $emailTeacher, $birth)";
+      $result = $db->query($sql);
       if($result)
         return 1;
       else
@@ -40,24 +39,23 @@
   }
 
   function getUserAttributes($db){
-    if($db->connect_error){
+    if($db->connect_error)
       die("Connection failed...". $db->connect_error);
-    }
     $active = $_SESSION['login_user'];
     $sql = "SELECT name, lastname, emailTeacher, birth FROM Teacher WHERE name='$active'";
     $result = $db->query($sql);
 
     if ($result->num_rows > 0){
-        while($value = $result->fetch_assoc()){
-          echo '<h3>Nombre:</h3>';
-          echo '<li>'.$value['name'].'</li>';
-          echo '<h3>Apellidos:</h3>';
-          echo '<li>'.$value['lastname'].'</li>';
-          echo '<h3>E-mail:</h3>';
-          echo '<li>'.$value['emailTeacher'].'</li>';
-          echo '<h3>Fecha de nacimiento:</h3>';
-          echo '<li>'.$value['birth'].'</li>';
-        }
+      while($value = $result->fetch_assoc()){
+        echo '<h3>Nombre:</h3>';
+        echo '<li>'.$value['name'].'</li>';
+        echo '<h3>Apellidos:</h3>';
+        echo '<li>'.$value['lastname'].'</li>';
+        echo '<h3>E-mail:</h3>';
+        echo '<li>'.$value['emailTeacher'].'</li>';
+        echo '<h3>Fecha de nacimiento:</h3>';
+        echo '<li>'.$value['birth'].'</li>';
+      }
     } else
       echo "0 results found...";
   }
