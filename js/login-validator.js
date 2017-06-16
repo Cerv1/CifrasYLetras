f3dd5fa3fdace64dc3c3467f4b34f3e96274f7d2
Cerv1 <cervick13@gmail.com>
2017-06-16 18:32:39 +0200

    finished login validator with js

--- js/login-validator.js
+++ js/login-validator.js
@@ -1,22 +1,28 @@
-function validateForm() {
-    var x = document.forms["login-form"]["user"].value;
-    var y = document.forms["login-form"]["pass"].value;
+  function validateForm() {
+    var email = document.forms["login-form"]["user"].value;
+    var password = document.forms["login-form"]["pass"].value;
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var errors = [];
 
-    if (x == "") {
-        document.getElementById('user-info').textContent='Usuario no válido';
-        return false;
-    }
-    else{
+    if (regex.test(email)) {
       document.getElementById('user-info').textContent='';
     }
+    else{
+      document.getElementById('user-info').textContent='Email no válido';
+      errors[errors.length] = "You must enter a valid email";
+    }
 
-    if (y == "") {
+    if (password == "") {
         document.getElementById('pass-info').textContent='Contraseña no válida';
-        return false;
+        errors[errors.length] = "You must enter a valid password";
     }
     else{
       document.getElementById('pass-info').textContent='';
     }
 
-    return true;
-}
+    if(errors.length > 0)
+      return false;
+    else
+      return true;
+
+  }
