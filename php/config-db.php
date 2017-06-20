8eb34a56c7e8955ffc086bd25dd00cb6f60afe75
Adrian Morente <adrian95morente@gmail.com>
2017-06-20 09:36:15 +0200

    cleaning

--- php/config-db.php
+++ php/config-db.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
   if(!defined('DB_SERVER'))
     define('DB_SERVER', 'localhost');
   if(!defined('DB_USERNAME'))
@@ -8,16 +7,5 @@
     define('DB_PASSWORD', 'twpassword');
   if(!defined('DB_DATABASE'))
     define('DB_DATABASE', 'CifrasYLetrasDB');
-=======
-  if(!defined("DB_SERVER"))
-    define('DB_SERVER', 'localhost');
-  if(!defined("DB_USERNAME"))
-    define('DB_USERNAME', 'root');
-  if(!defined("DB_PASSWORD"))
-    define('DB_PASSWORD', 'twpassword');
-  if(!defined("DB_DATABASE"))
-    define('DB_DATABASE', 'CifrasYLetrasDB');
-    
->>>>>>> master
   $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
  ?>
