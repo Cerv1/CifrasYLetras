0117ec3ca6f6f0819a4504cfacdde8839971f161
Cerv1 <cervick13@gmail.com>
2017-06-20 18:12:12 +0200

    Finishing documentation

--- php/config-db.php
+++ php/config-db.php
@@ -2,11 +2,11 @@
   if(!defined("DB_SERVER"))
     define('DB_SERVER', 'localhost');
   if(!defined("DB_USERNAME"))
-    define('DB_USERNAME', 'root');
+    define('DB_USERNAME', 'cervietsiit1617');
   if(!defined("DB_PASSWORD"))
-    define('DB_PASSWORD', 'twpassword');
+    define('DB_PASSWORD', 'WC869E');
   if(!defined("DB_DATABASE"))
-    define('DB_DATABASE', 'CifrasYLetrasDB');
-    
+    define('DB_DATABASE', 'cervietsiit1617');
+
   $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
  ?>
