3b994548a01e8c201ee70cca47e0d050845af9a3
Cerv1 <cervick13@gmail.com>
2017-06-20 09:31:59 +0200

    minor fixes

--- php/config-db.php
+++ php/config-db.php
@@ -1,7 +1,12 @@
 <?php
-  define('DB_SERVER', 'localhost');
-  define('DB_USERNAME', 'root');
-  define('DB_PASSWORD', 'twpassword');
-  define('DB_DATABASE', 'CifrasYLetrasDB');
+  if(!defined("DB_SERVER"))
+    define('DB_SERVER', 'localhost');
+  if(!defined("DB_USERNAME"))
+    define('DB_USERNAME', 'root');
+  if(!defined("DB_PASSWORD"))
+    define('DB_PASSWORD', 'twpassword');
+  if(!defined("DB_DATABASE"))
+    define('DB_DATABASE', 'CifrasYLetrasDB');
+    
   $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
  ?>
